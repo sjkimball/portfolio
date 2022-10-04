2b54dd72a02659f59aa042026c1664a298a48a37
Sam Kimball <samuel.j.kimball@gmail.com>
2022-10-04 11:50:50 -0500

    Remove buildHooks from dashboard

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -15,45 +15,31 @@ export default {
                   buildHookId: '5ec419820244c70183681e4f',
                   title: 'Frontend',
                   name: 'sjkimball',
-                  apiId: '30ae73c5-72e5-468b-9083-c9f60e9c8752'
+                  apiId: '30ae73c5-72e5-468b-9083-c9f60e9c8752',
                 },
                 {
                   buildHookId: '5ec4198043537101c3a54b36',
                   title: 'Studio',
                   name: 'sjkimball-portfolio',
-                  apiId: 'ee7886a0-c036-4516-be96-6c456bf9c7bf'
-                },
-                {
-                  buildHookId: '5ec419820244c70183681e4f',
-                  title: 'Frontend - Staging',
-                  name: 'sjkimball',
-                  apiId: '30ae73c5-72e5-468b-9083-c9f60e9c8752',
-                  branch: 'staging'
-                },
-                {
-                  buildHookId: '5ec4198043537101c3a54b36',
-                  title: 'Studio - Staging',
-                  name: 'sjkimball-portfolio',
                   apiId: 'ee7886a0-c036-4516-be96-6c456bf9c7bf',
-                  branch: 'staging'
-                }
-              ]
-            }
-          }
+                },
+              ],
+            },
+          },
         ],
         data: [
           {
             title: 'GitHub repo',
             value: 'https://github.com/sjkimball/portfolio',
-            category: 'Code'
+            category: 'Code',
           },
           {
             title: 'Frontend',
             value: 'https://sjkimball.me/',
-            category: 'apps'
-          }
-        ]
-      }
+            category: 'apps',
+          },
+        ],
+      },
     },
     { name: 'project-users', layout: { height: 'auto' } },
     {
@@ -61,9 +47,9 @@ export default {
       options: {
         title: 'Recent projects',
         order: '_createdAt desc',
-        types: ['project']
+        types: ['project'],
       },
-      layout: { width: 'medium' }
-    }
-  ]
+      layout: { width: 'medium' },
+    },
+  ],
 };

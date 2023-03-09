5be8b766096260990491698064eb26356d9de30b
Sam Kimball <samuel.j.kimball@gmail.com>
2023-03-09 02:00:50 -0600

    Clean up

--- web/src/templates/profile.js
+++ web/src/templates/profile.js
@@ -6,52 +6,32 @@ import Layout from '../components/layout';
 import Profile from '../components/profile';
 
 const ProfileTemplate = ({ data }) => {
-	const profile = data.profile;
-	const relatedProjects = data.relatedProjects.edges
+  const profile = data.profile;
 
-	return (
-		<Layout>
-			<Profile profile={profile} relatedProjects={relatedProjects} />
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Profile profile={profile} />
+    </Layout>
+  );
 };
 
 export const query = graphql`
-	query ($slug: String!) {
-	  profile: sanityPerson(slug: {current: {eq: $slug}}) {
-	    profileImg {
-				...imageData
-	    }
-	    name
-	    office {
-	    	contactInfo {
-	    		address {
-	    			city
-	    		}
-	    	}
-	    }
-	    _rawBio
-	    _id
-		}
-		relatedProjects: allSanityProject(filter: {projectMembers: {elemMatch: {person: {slug: {current: {eq: $slug}}}}}}) {
-			edges {
-				node {
-					id
-					client {
-						name
-						slug {
-							current
-						}
-					}
-					coverImg {
-						...imageData
-					}
-					slug {
-						current
-					}
-				}
-			}
-		}
+  query ($slug: String!) {
+    profile: sanityPerson(slug: { current: { eq: $slug } }) {
+      profileImg {
+        ...imageData
+      }
+      name
+      office {
+        contactInfo {
+          address {
+            city
+          }
+        }
+      }
+      _rawBio
+      _id
+    }
   }
 `;
 
--- web/src/templates/project.js
+++ web/src/templates/project.js
@@ -6,49 +6,45 @@ import Layout from '../components/layout';
 import Project from '../components/project';
 
 const ProjectTemplate = ({ data }) => {
-	const project = data.sanityProject;
+  const project = data.sanityProject;
 
-	return (
-		<Layout>
-			<Project
-				project={project}
-			/>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Project project={project} />
+    </Layout>
+  );
 };
 
 export const query = graphql`
-	query ($slug: String!) {
-		sanityProject(slug: {current: {eq: $slug}}) {
-		  title
-		  sector
-		  projectSummary
-		  _rawProjectDesc(resolveReferences: {maxDepth: 10})
-		  coverImg {
-				...imageData
-		  }
-		  productImgs {
-				...imageData
-		  }
-		  client {
-		    name
-		  }
-		  office {
-		  	contactInfo {
-			    address {
-			    	city
-			    }
-		  	}
-		  }
-		  disciplines
-		  projectMembers {
-		  	person {
-					name
-				}
-		  	_key
-		  }
-		}
-	}
+  query ($slug: String!) {
+    sanityProject(slug: { current: { eq: $slug } }) {
+      title
+      sector
+      projectSummary
+      _rawProjectDesc(resolveReferences: { maxDepth: 10 })
+      coverImg {
+        ...imageData
+      }
+      productImgs {
+        ...imageData
+      }
+      client {
+        name
+      }
+      office {
+        contactInfo {
+          address {
+            city
+          }
+        }
+      }
+      disciplines
+      projectMembers {
+        name
+        _key
+      }
+    }
+  }
 `;
 
 export default ProjectTemplate;

8bfb3999174d72639713bd949ba537c984c707f1
Sam Kimball <samuel.j.kimball@gmail.com>
2023-03-09 02:00:39 -0600

    Update index structure, create page template, layout container

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,57 +1,12 @@
-import React, { useEffect } from 'react';
-import { gsap } from 'gsap';
-
-import Layout from '../components/layout';
-import SEO from '../components/seo';
-import HeroSvg from '../components/astronaut';
+import React from 'react';
 
 import useLandingPage from '../hooks/use-landing-page';
-
-import './pages.css';
-
-import { messageTl } from '../animations/hero-animation';
-
-const isDisabled = (item) => {
-  return !item.disabled;
-};
-const isFigure = (item) => {
-  return item._type == 'figure';
-};
+import Page from '../templates/page';
 
 const IndexPage = () => {
-  const { content } = useLandingPage();
+  const data = useLandingPage();
 
-  useEffect(() => {
-    messageTl();
-  });
-
-  return (
-    <Layout>
-      <SEO title="Home" />
-      <article className={`rec-article rec-landing`}>
-        <div className={'rec-landing__headline'}>
-          <h2>
-            <span className={`rec-headline__line`}>
-              Hey<span className={`rec-headline__punc`}>.&nbsp;</span>
-            </span>
-            <span className={`rec-headline__line`}>
-              I'm Sam<span className={`rec-headline__punc`}>.</span>
-            </span>
-          </h2>
-          <ul>
-            <li>UX/UI Designer</li>
-            <li>Front End Dev</li>
-            <li>Graphic Artist</li>
-          </ul>
-        </div>
-        <HeroSvg
-          className={`rec-hero`}
-          imageAsset={figures[0]}
-          imageType={`hero`}
-        />
-      </article>
-    </Layout>
-  );
+  return <Page data={data} />;
 };
 
 export default IndexPage;

c4f4dc4e588ba3d93e1a1c7d8764f45e6635bf5d
Sam Kimball <samuel.j.kimball@gmail.com>
2023-03-15 15:57:33 -0500

    Rename pages for testing

---
+++ web/src/pages/contactsomething.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import { graphql } from 'gatsby';
+
+import SEO from '../components/seo';
+import Layout from '../components/layout';
+import OfficeContact from '../components/office-contact';
+
+import './pages.css';
+
+const ContactPage = ({ data }) => {
+  console.log('data', data);
+  const offices = data.offices.edges;
+  return (
+    <Layout>
+      <SEO title="Contact" />
+      <article className={`rec-article rec-contact`}>
+        <header className={`rec-article__header rec-contact__header`}>
+          <h2>Contact</h2>
+        </header>
+        <section className={`rec-preview-container`}>
+          {offices.map(({ node: office }, index) => {
+            return <OfficeContact key={index} office={office} />;
+          })}
+        </section>
+      </article>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query OfficeContact {
+    offices: allSanityOffice(sort: { fields: contactInfo___address___city }) {
+      edges {
+        node {
+          _id
+          images {
+            ...imageData
+          }
+          contactInfo {
+            address {
+              city
+              street
+              streetSecondary
+              stateProvince
+              postalCode
+              country
+            }
+            email
+            phone
+          }
+          description
+        }
+      }
+    }
+  }
+`;
+
+export default ContactPage;

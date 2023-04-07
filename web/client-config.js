f2bb0d0b4fc37c531d6f1e18d30e2cbb83f0bf9e
Sam Kimball <samuel.j.kimball@gmail.com>
2023-04-07 18:59:59 -0500

    Update client config

--- web/client-config.js
+++ web/client-config.js
@@ -2,5 +2,7 @@ module.exports = {
   sanity: {
     projectId: process.env.GATSBY_SANITY_PROJECT_ID,
     dataset: process.env.GATSBY_SANITY_DATASET || 'production',
+    useCdn: false,
+    apiVersion: '2023-03-30',
   },
 };

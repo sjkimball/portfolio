4b6e492f686e0bed6217ca64aa5c5dac47b0e31b
Sam Kimball <samuel.j.kimball@gmail.com>
2023-04-09 17:05:00 -0500

    Update graphql fragments to include menu items

--- web/src/graphql-fragments/pageInfo.js
+++ web/src/graphql-fragments/pageInfo.js
@@ -3,7 +3,10 @@ import { graphql } from 'gatsby';
 export const PageInfo = graphql`
   fragment PageInfo on SanityPage {
     id
-    _rawContent(resolveReferences: { maxDepth: 10 })
     title
+    navMenu {
+      ...NavMenu
+    }
+    _rawContent(resolveReferences: { maxDepth: 10 })
   }
 `;

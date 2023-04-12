551788d2beef77e7332646c56b4fa7b7ccc8f39d
Sam Kimball <samuel.j.kimball@gmail.com>
2023-04-11 23:12:59 -0500

    Update date formatting

--- web/src/components/post-preview.js
+++ web/src/components/post-preview.js
@@ -5,18 +5,16 @@ import { Link } from 'gatsby';
 import PreviewImage from '../components/preview-image';
 
 import { format, parseISO } from 'date-fns';
-import { enUS } from 'date-fns/locale';
 
 import './preview.css';
 
 const PostPreview = (props) => {
   const { coverImg, publishedAt, title, slug, subtitle } = props;
-  const dateForTime = parseISO(publishedAt);
-  const formattedDate = format(new Date(publishedAt), 'MMMM d, yyyy');
+  const formattedDate = format(parseISO(publishedAt), 'MMMM d, yyyy');
   return (
     <Link to={`/blog/${slug.current}`} className={`preview preview--light`}>
       <h4>{title}</h4>
-      <time dateTime={dateForTime}>{formattedDate}</time>
+      <time dateTime={formattedDate}>{formattedDate}</time>
       <p>{subtitle}</p>
       {<PreviewImage imageAsset={coverImg} />}
     </Link>

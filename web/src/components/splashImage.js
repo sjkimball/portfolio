a92a9529f497f3b0b234e5e82142519f8a33158a
Sam Kimball <samuel.j.kimball@gmail.com>
2024-01-12 19:16:52 -0600

    Add figure wrapper to splash image

--- web/src/components/splashImage.js
+++ web/src/components/splashImage.js
@@ -6,37 +6,46 @@ const SplashImage = (props) => {
   const { messageOverlay: message, image } = props;
   const imageUrl = imageUrlFor(image);
   return (
-    <svg
-      id="splashSvg"
-      viewBox="0 0 2000 3000"
-      preserveAspectRatio="xMidYMid meet"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-        backgroundPosition: 'center center',
-        backgroundSize: 'cover',
-      }}
-    >
-      <defs>
-        <mask id="imageMask">
-          <rect width="100%" height="100%" rx="10%" fill="white" />
-        </mask>
-        <filter id="floodFilter" filterUnits="userSpaceOnUse">
-          <feFlood floodColor="blue" floodOpacity="0.5" />
-        </filter>
-      </defs>
+    <figure className="splashFigure">
+      <svg
+        id="splashSvg"
+        viewBox="0 0 3000 3000"
+        preserveAspectRatio="xMidYMid meet"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+          backgroundPosition: 'center center',
+          backgroundSize: 'cover',
+        }}
+      >
+        <defs>
+          <mask id="imageMask">
+            <rect width="100%" height="100%" fill="white" />
+            <text
+              x="50%"
+              y="50%"
+              fill="black"
+              fontSize="50vw"
+              fontWeight="bold"
+              textAnchor="middle"
+              dominantBaseline="central"
+            >
+              {message}
+            </text>
+          </mask>
+          <filter id="floodFilter">
+            <feFlood floodColor="blue" floodOpacity="0.5" />
+          </filter>
+        </defs>
 
-      {/* <image
-        className={`something`}
-        href={imageUrlFor(image)}
-        mask="url(#imageMask)"
-      /> */}
-      <rect
-        width="100%"
-        height="100%"
-        filter="url(#floodFilter)"
-        mask="url(#imageMask)"
-      />
-    </svg>
+        <rect
+          width="100%"
+          height="100%"
+          filter="url(#floodFilter)"
+          mask="url(#imageMask)"
+        />
+      </svg>
+      <figcaption>This is the splash image.</figcaption>
+    </figure>
   );
 };
 export default SplashImage;

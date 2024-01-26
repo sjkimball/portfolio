4635a1930b33e274bd2ced47c970320419baec4a
Sam Kimball <samuel.j.kimball@gmail.com>
2024-01-25 22:42:12 -0600

    Update gatsby-node

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -8,7 +8,7 @@ async function createLandingPages(
   pathPrefix = '/',
   graphql,
   actions,
-  reporter
+  reporter,
 ) {
   const { createPage } = actions;
   const result = await graphql(`
@@ -85,49 +85,48 @@ async function createProjects(pathPrefix = '/', graphql, actions, reporter) {
   });
 }
 
-// Following is removed until need for blog.
-// async function createPosts(pathPrefix = '/', graphql, actions, reporter) {
-//   const { createPage } = actions;
-//   const result = await graphql(`
-//     {
-//       allSanityPost {
-//         edges {
-//           node {
-//             id
-//             slug {
-//               current
-//             }
-//           }
-//         }
-//       }
-//       parentRoute: sanityRoute(page: { title: { eq: "Blog" } }) {
-//         id
-//       }
-//     }
-//   `);
+async function createPosts(pathPrefix = '/', graphql, actions, reporter) {
+  const { createPage } = actions;
+  const result = await graphql(`
+    {
+      allSanityPost {
+        edges {
+          node {
+            id
+            slug {
+              current
+            }
+          }
+        }
+      }
+      parentRoute: sanityRoute(page: { title: { eq: "Blog" } }) {
+        id
+      }
+    }
+  `);
 
-//   if (result.errors) throw result.errors;
+  if (result.errors) throw result.errors;
 
-//   const postEdges = (result.data.allSanityPost || {}).edges || [];
-//   const parentRoute = result.data.parentRoute;
-//   postEdges.forEach((edge) => {
-//     const { id, slug = {} } = edge.node;
-//     const parentRouteID = parentRoute.id;
-//     const path = `${pathPrefix}/${slug.current}/`;
-//     reporter.info(`Creating post page: ${path}`);
-//     createPage({
-//       path,
-//       component: require.resolve('./src/templates/post.js'),
-//       context: { id, parentRouteID },
-//     });
-//   });
-// }
+  const postEdges = (result.data.allSanityPost || {}).edges || [];
+  const parentRoute = result.data.parentRoute;
+  postEdges.forEach((edge) => {
+    const { id, slug = {} } = edge.node;
+    const parentRouteID = parentRoute.id;
+    const path = `${pathPrefix}/${slug.current}/`;
+    reporter.info(`Creating post page: ${path}`);
+    createPage({
+      path,
+      component: require.resolve('./src/templates/post.js'),
+      context: { id, parentRouteID },
+    });
+  });
+}
 
 async function createProfiles(pathPrefix = '/', graphql, actions, reporter) {
   const { createPage } = actions;
   const result = await graphql(`
     {
-      allSanityPerson(filter: { featured: { eq: true } }) {
+      allSanityPerson(filter: { slug: { current: { ne: null } } }) {
         edges {
           node {
             id
@@ -162,6 +161,6 @@ async function createProfiles(pathPrefix = '/', graphql, actions, reporter) {
 exports.createPages = async ({ graphql, actions, reporter }) => {
   await createLandingPages('', graphql, actions, reporter);
   await createProjects('/work', graphql, actions, reporter);
-  // await createPosts('/blog', graphql, actions, reporter);
+  await createPosts('/blog', graphql, actions, reporter);
   await createProfiles('/about', graphql, actions, reporter);
 };

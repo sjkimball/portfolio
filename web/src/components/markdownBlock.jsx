58dfb921f6d1d000e325683eb6881ae46c53961d
Sam Kimball <samuel.j.kimball@gmail.com>
2024-05-03 19:36:49 -0500

    Update Markdown block

--- web/src/components/markdownBlock.jsx
+++ web/src/components/markdownBlock.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+
 import Markdown from 'react-markdown';
 
-import './markdownBlock.css';
+import './MarkdownBlock.css';
 
 const MarkdownBlock = (props) => {
+  console.log('props in markdown block', props);
   const { title, mdBlockContent: markdown } = props;
   return (
     <section className="markdown markdown-block">
+      {title ? <h3>{title}</h3> : ''}
       <Markdown>{markdown}</Markdown>
     </section>
   );

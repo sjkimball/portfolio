9fb20c0938481c91838725f05b3b1c639d36d541
Sam Kimball <samuel.j.kimball@gmail.com>
2024-05-29 16:18:07 -0500

    Remove unused files

--- web/src/components/portableText/blocks/Person.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Profile from '../../profile';
-
-const PersonBlock = (props) => {
-  const { value } = props;
-  return <Profile {...value.person} />;
-};
-
-export default PersonBlock;

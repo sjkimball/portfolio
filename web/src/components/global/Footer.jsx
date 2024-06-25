dfff7ef6080eb535507d947b1f7ebeb85065683b
Sam Kimball <samuel.j.kimball@gmail.com>
2024-06-25 18:12:17 -0500

    Layout max widths

--- web/src/components/global/Footer.jsx
+++ web/src/components/global/Footer.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import { footer, footerLinks, copyright, logo } from './Footer.module.css';
+import {
+  footer,
+  container,
+  footerLinks,
+  copyright,
+  logo,
+} from './Footer.module.css';
 
 const Footer = (props) => {
   // console.dir('props in footer', props);
@@ -12,30 +18,32 @@ const Footer = (props) => {
       data-theme="dark"
       className={`mainFooter ${footer}`}
     >
-      <div className={logo}>SK</div>
-      <ul className={footerLinks}>
-        {links.map((item, i) =>
-          item._type == 'link.internal' ? (
-            <li key={i}>
-              <Link to={`/${item.reference.slug.current}`} className={link}>
-                {item.title}
-              </Link>
-            </li>
-          ) : item._type == 'link.external' ? (
-            <li key={i}>
-              <a
-                href={item.url}
-                target={item.newWindow == true ? '_blank' : ''}
-              >
-                {item.title}
-              </a>
-            </li>
-          ) : (
-            ''
-          ),
-        )}
-      </ul>
-      <small className={copyright}>{text}</small>
+      <div className={`${container}`}>
+        <div className={logo}>SK</div>
+        <ul className={footerLinks}>
+          {links.map((item, i) =>
+            item._type == 'link.internal' ? (
+              <li key={i}>
+                <Link to={`/${item.reference.slug.current}`} className={link}>
+                  {item.title}
+                </Link>
+              </li>
+            ) : item._type == 'link.external' ? (
+              <li key={i}>
+                <a
+                  href={item.url}
+                  target={item.newWindow == true ? '_blank' : ''}
+                >
+                  {item.title}
+                </a>
+              </li>
+            ) : (
+              ''
+            ),
+          )}
+        </ul>
+        <small className={copyright}>{text}</small>
+      </div>
     </footer>
   );
 };

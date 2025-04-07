2ae0c4217dbbb6228022948997aa0731b542086c
Sam Kimball <samuel.j.kimball@gmail.com>
2025-04-07 17:39:31 -0500

    Update fields in seo.home

--- studio/src/schemas/objects/seo/home.js
+++ studio/src/schemas/objects/seo/home.js
@@ -16,21 +16,6 @@ export default {
           .max(50)
           .warning('Longer titles may be truncated by search engines'),
     },
-    {
-      name: 'description',
-      title: 'Description',
-      type: 'text',
-      rows: 2,
-      validation: (rule) =>
-        rule
-          .max(150)
-          .warning('Longer descriptions may be truncated by search engines'),
-    },
-    {
-      name: 'image',
-      title: 'Image',
-      type: 'image',
-    },
   ],
   validation: (rule) => rule.required(),
 };

29f11652a760d874d1bbd07d0123a14b1f08d95d
Sam Kimball <samuel.j.kimball@gmail.com>
2025-04-07 18:00:11 -0500

    Consolidate Hero components

--- web/src/components/heroes/Page.jsx
+++ web/src/components/heroes/Page.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 
 import ImageModule from '../portableText/blocks/Image';
 
-import {} from './Hero.module.css';
+import { hero } from './Hero.module.css';
 
 const HeroPage = (props) => {
   // console.dir('props in HeroPage', props.content[0]);
-  const { title, content } = props;
+  const { title, subtitle, content } = props;
   const imageAsset = content[0];
 
   return (
-    <section id="heroPage">
-      <h1>{title}</h1>
+    <section id="heroPage" className={`hero ${hero}`}>
+      <hgroup>
+        <h1>{title}</h1>
+        <p>{subtitle}</p>
+      </hgroup>
       {content.length !== 0 ? <ImageModule {...content[0]} /> : ''}
     </section>
   );

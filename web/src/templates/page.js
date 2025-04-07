5da560dc44639d0a05fd08ee58f58ba6b266c711
Sam Kimball <samuel.j.kimball@gmail.com>
2025-04-07 18:01:39 -0500

    Update queries

--- web/src/templates/page.js
+++ web/src/templates/page.js
@@ -3,13 +3,9 @@ import React from 'react';
 import { graphql } from 'gatsby';
 
 import GraphQLErrorList from '../components/GraphqlErrorList';
-import HeroHome from '../components/heroes/Home';
 import HeroPage from '../components/heroes/Page';
 import Layout from '../components/global/Layout';
 import PortableText from '../components/portableText/PortableText';
-import SectionBlock from '../components/portableText/blocks/SectionBlock';
-import ProjectsBlock from '../components/portableText/blocks/ProjectsBlock';
-import GridBlock from '../components/portableText/blocks/GridBlock';
 import SEO from '../components/Seo';
 
 export const query = graphql`
@@ -18,7 +14,7 @@ export const query = graphql`
       ...pageData
     }
     site: sanitySettingsSite(_id: { regex: "/(drafts.|)settings/" }) {
-      ...settingsSiteData
+      ...settingsSite
     }
   }
 `;
@@ -50,13 +46,15 @@ const Page = (props) => {
     );
   }
 
-  // Page Content
-  const page = data.page;
+  let page = {};
+
+  data.page._id && data.page._id == 'home'
+    ? (page = data.page.homePage)
+    : (page = data.page);
+
   // Hero
   const hero =
-    page._id && page._id == 'home' ? (
-      <HeroHome {...page.hero} />
-    ) : page.showHero && page.showHero == true ? (
+    page.showHero && page.showHero == true ? (
       <HeroPage {...page.hero} />
     ) : (
       <header>
@@ -64,33 +62,11 @@ const Page = (props) => {
       </header>
     );
 
-  //Home Page Content
-  const modules = (page._rawContent || [])
-    .filter((c) => !c.disabled)
-    .map((c, i) => {
-      let el = null;
-      switch (c._type) {
-        case 'module.projects':
-          el = <ProjectsBlock key={c._key} {...c} />;
-          break;
-        case 'module.grid':
-          el = <GridBlock key={c._key} {...c} />;
-          break;
-        case 'module.section':
-          el = <SectionBlock key={c._key} {...c} />;
-          break;
-        default:
-          el = null;
-      }
-      return el;
-    });
-
   const body = page._rawBody || [];
 
   return (
     <Layout site={site}>
       {hero}
-      {modules}
       {body.length > 0 ? <PortableText blocks={body} /> : ''}
     </Layout>
   );
--- web/src/templates/project.js
+++ web/src/templates/project.js
@@ -35,7 +35,7 @@ export const query = graphql`
       }
     }
     site: sanitySettingsSite(_id: { regex: "/(drafts.|)settings/" }) {
-      ...settingsSiteData
+      ...settingsSite
     }
   }
 `;

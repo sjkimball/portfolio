58d5c81ebdf5429ec4920c9cdd57ab4a67f2501e
Sam Kimball <samuel.j.kimball@gmail.com>
2025-04-07 17:43:15 -0500

    Update fields in hero.page

--- studio/src/schemas/objects/hero/page.js
+++ studio/src/schemas/objects/hero/page.js
@@ -2,13 +2,31 @@ export default {
   name: 'hero.page',
   title: 'Page hero',
   type: 'object',
+  options: {
+    collapsible: true,
+    collapsed: false,
+  },
   fields: [
     // Title
     {
       name: 'title',
       title: 'Title',
+      type: 'string',
+    },
+    // Subtitle
+    {
+      name: 'subtitle',
+      title: 'Subtitle',
       type: 'text',
       rows: 3,
+    },
+    // Links
+    {
+      name: 'links',
+      title: 'Link',
+      type: 'array',
+      of: [{ type: 'link.internal' }, { type: 'link.external' }],
+      validation: (rule) => rule.max(1),
     },
     // Content
     {

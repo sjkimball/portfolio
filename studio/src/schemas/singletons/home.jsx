f2bbe0805065795841e23c9a05b1550f28d7b6f1
Sam Kimball <samuel.j.kimball@gmail.com>
2025-04-07 17:44:15 -0500

    Update Home singleton

--- studio/src/schemas/singletons/home.jsx
+++ studio/src/schemas/singletons/home.jsx
@@ -7,41 +7,23 @@ export default {
   title: TITLE,
   type: 'document',
   icon: HomeIcon,
-  groups: [
-    {
-      default: true,
-      name: 'editorial',
-      title: 'Editorial',
-    },
-    {
-      name: 'seo',
-      title: 'SEO',
-    },
-  ],
   fields: [
     {
-      name: 'hero',
-      title: 'Hero',
-      type: 'hero.home',
-      group: 'editorial',
-    },
-    {
-      name: 'content',
-      title: 'Modules',
-      type: 'array',
-      of: [
-        { type: 'module.section' },
-        { type: 'module.images' },
-        { type: 'module.grid' },
-        { type: 'module.projects' },
-      ],
-      group: 'editorial',
+      name: 'homePage',
+      title: 'Page',
+      type: 'reference',
+      description: 'Page must be published to use as home page.',
+      to: [{ type: 'page' }],
+      options: {
+        disableNew: true,
+        filter: 'useAsHomePage == $useAsHomePage',
+        filterParams: { useAsHomePage: true },
+      },
     },
     {
       name: 'seo',
       title: 'SEO',
       type: 'seo.home',
-      group: 'seo',
     },
   ],
   preview: {

1d963f409a571a0d7768726f99c2a7e45e727062
Sam Kimball <samuel.j.kimball@gmail.com>
2025-04-07 17:49:30 -0500

    Update site settings hook

--- web/src/hooks/use-site-settings.js
+++ web/src/hooks/use-site-settings.js
@@ -3,13 +3,13 @@ import { graphql, useStaticQuery } from 'gatsby';
 const useSiteSettings = () => {
   const data = useStaticQuery(graphql`
     query {
-      site: sanitySettingsSite(_id: { regex: "/(drafts.|)settings/" }) {
-        ...settingsSiteData
+      settings: sanitySettingsSite(_id: { regex: "/(drafts.|)settings/" }) {
+        ...settingsSite
       }
     }
   `);
 
-  return data;
+  return data.settings;
 };
 
 export default useSiteSettings;
